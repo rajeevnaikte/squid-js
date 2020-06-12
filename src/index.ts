45b9bead7a7cedba2db517006b4a8fcd0266ae2d
rajeevnaikte <rajeevnaikte@gmail.com>
2020-06-11 20:30:33 -0400

    fix window not found error

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { ViewModel } from './model/ViewModel';
 import { ViewState } from './model/ViewState';
 import { Component } from './model/Component';
 
-if (window) {
+if (typeof window !== 'undefined') {
   // @ts-ignore
   window.UX = UX;
   // @ts-ignore
